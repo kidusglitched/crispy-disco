1ba356ba6a0642e298f856462cc3a4ca289a4d8b
kidusglitched <meskiduse@gmail.com>
2025-10-28 17:24:52 +0300

    Clean telegram packages and run main.py
    Remove telegram packages before running main.py

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 set -e
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+rm -rf /opt/render/project/src/.venv/lib/python3.13/site-packages/telegram*
 python3 main.py
